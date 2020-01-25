7614feccbad4410b771d97719d115af159a00543
Asha Fernandez <ash.fernan90@gmail.com>
2020-01-25 22:01:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
-  attr_accessor :status
   attr_reader :amount, :sender, :receiver
-  
+  attr_accessor :status
+
   def initialize(sender, receiver, amount)
     @status = "pending"
     @sender = sender
